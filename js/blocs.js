feb29e378858139469c497cf481dd718098bd4a6
xavius06 <xav.robart@gmail.com>
2021-05-24 11:03:35 +0200

    mise en place url page

--- js/blocs.js
+++ js/blocs.js
@@ -192,7 +192,7 @@ $(document).ready(function () {
             scrollTop: $("#scroll-hero").closest(".bloc").height()
         }, "slow")
     }), extraNavFuncs(), setUpSpecialNavs(), setUpDropdownSubs(), setUpLightBox(), setUpVisibilityToggle(), addSwipeSupport(), addKeyBoardSupport(), -1 != navigator.userAgent.indexOf("Safari") && -1 == navigator.userAgent.indexOf("Chrome") && $("#page-loading-blocs-notifaction").remove()
-}), $(window).load(function () {
+}), $(window).load(function() {
     setFillScreenBlocHeight(), animateWhenVisible(), $("#page-loading-blocs-notifaction").remove()
 }).resize(function () {
     setFillScreenBlocHeight()
